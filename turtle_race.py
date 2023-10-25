9640fece1814e86d956cf01e9a2f0872b18d317e
vayola pradeep <102567147+sinonagar123@users.noreply.github.com>
2023-10-25 17:21:39 -0400

    Add files via upload

---
+++ turtle_race.py
@@ -0,0 +1,19 @@
+import turtle
+import random
+a = turtle. Turtle ()
+a.color ('red')
+b = turtle. Turtle ()
+b.color ('blue')
+c = turtle. Turtle ()
+c.color ('purple')
+turtle = [a, b, c]
+for item in turtle:
+  item.penup ()
+  item.shape ('turtle')
+  item.shapesize (4)
+a.goto (-300, -100)
+b.goto (-300,0)
+c. goto (-300, 100)
+for race in range (100):
+  for item in turtle:
+    item.fd (random. randint (0, 12))
